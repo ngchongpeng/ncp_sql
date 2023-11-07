8228ec2e3aa37b1e92a07ca2fb58be4c9a5b5139
chong <ngchongpeng@hotmail.com>
2023-11-07 16:24:02 +0800

    generateDynamicInsertScripts.sql

--- generateDynamicInsertScripts.sql
+++ generateDynamicInsertScripts.sql
@@ -1,9 +1,11 @@
 
+
+
 --
 if (object_id('tempdb..##temp')) is not null drop table ##temp
 
 --
-declare @query varchar(max) = 'select * from Person where Name=''Jingwen'''
+declare @query varchar(max) = 'select * from ParenthoodTaxRebateTransaction where ParenthoodTaxRebateHeaderId in (select Id from ParenthoodTaxRebateHeader where ParenthoodTaxRebateAccountId=''75CE502A-644E-4F3E-B7EA-0BCED8ADC120'')'
 declare @q nvarchar(max) = concat('select * into ##temp from (',  @query, ') a')
 exec sp_executesql @q
 
@@ -17,12 +19,18 @@ declare @table varchar(max) = trim(substring(
 		else charindex('where',@query)-(charindex('from',@query)+len('from'))
 	end))
 
---
-declare @columns varchar(max) = (select string_agg(COLUMN_NAME,',') from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME=@table)
-declare @values varchar(max) = (select string_agg(''''''''' + ' + 'isnull(convert(varchar(max),' + COLUMN_NAME + '),''NULL'')' + ' + ''''''''',','','',') from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME=@table)
+declare @t varchar(max) = case
+	when CHARINDEX('.',@table) = 0
+	then @table
+	else SUBSTRING(@table,charindex('.',@table)+1,len(@table)-charindex('.',@table))
+	end
 
 --
-declare @dynamicQuery nvarchar(max) = 'select ''insert into ' + @table + ' (' + @columns + ') values ('' + ' + 'concat(' + @values + ') + '')'' from ##temp'
+declare @columns varchar(max) = (select string_agg(COLUMN_NAME,',') from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME=@t)
+declare @values varchar(max) = (select string_agg(''''''''' + ' + 'isnull(convert(varchar(max),' + COLUMN_NAME + '),''NULL'')' + ' + ''''''''',','','',') from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME=@t)
+
+--
+declare @dynamicQuery nvarchar(max) = 'select replace(''insert into ' + @table + ' (' + @columns + ') values ('' + ' + 'concat(' + @values + ') + '')'',''''''NULL'''''',''NULL'') as [INSERT] from ##temp'
 exec sp_executesql @dynamicQuery
 
 --
