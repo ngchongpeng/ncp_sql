19c2ce69a744a2a506b80aa8b9b0174ac43d9d19
chong <ngchongpeng@hotmail.com>
2023-11-23 09:10:07 +0800

    fix script.

--- generateDynamicInsertScripts.sql
+++ generateDynamicInsertScripts.sql
@@ -4,10 +4,18 @@
 --
 if (object_id('tempdb..##temp')) is not null drop table ##temp
 
+
+
 --
-declare @query varchar(max) = 'select * from ParenthoodTaxRebateTransaction where ParenthoodTaxRebateHeaderId in (select Id from ParenthoodTaxRebateHeader where ParenthoodTaxRebateAccountId=''75CE502A-644E-4F3E-B7EA-0BCED8ADC120'')'
+declare @query varchar(max) = 'query'
+
+
+
+--
 declare @q nvarchar(max) = concat('select * into ##temp from (',  @query, ') a')
 exec sp_executesql @q
+
+
 
 --
 declare @table varchar(max) = trim(substring(
@@ -25,14 +33,22 @@ declare @t varchar(max) = case
 	else SUBSTRING(@table,charindex('.',@table)+1,len(@table)-charindex('.',@table))
 	end
 
---
-declare @columns varchar(max) = (select string_agg(COLUMN_NAME,',') from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME=@t)
-declare @values varchar(max) = (select string_agg(''''''''' + ' + 'isnull(convert(varchar(max),' + COLUMN_NAME + '),''NULL'')' + ' + ''''''''',','','',') from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME=@t)
+
 
 --
-declare @dynamicQuery nvarchar(max) = 'select replace(''insert into ' + @table + ' (' + @columns + ') values ('' + ' + 'concat(' + @values + ') + '')'',''''''NULL'''''',''NULL'') as [INSERT] from ##temp'
+declare @columns varchar(max) = (select string_agg(COLUMN_NAME,',') from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME=@t and COLUMN_NAME != 'RowVersion')
+select @columns
+declare @values varchar(max) = (select string_agg(''''''''' + ' + 'isnull(convert(varchar(max),' + '[' + COLUMN_NAME + ']' + '),''NULL'')' + ' + ''''''''',','','',') from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME=@t and COLUMN_NAME != 'RowVersion')
+
+
+
+
+--
+declare @dynamicQuery nvarchar(max) = 'select ''insert into ' + @table + ' (' + @columns + ') values ('' + ' + 'concat(' + @values + ') + '')'' from ##temp'
+select @dynamicQuery
 exec sp_executesql @dynamicQuery
+
+
 
 --
 if (object_id('tempdb..##temp')) is not null drop table ##temp
-
